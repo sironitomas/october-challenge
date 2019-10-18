d57eda3a5c3b925c928d03fa898d3fed1c6a7ed2
Tomas Sironi <sironitomas@gmail.com>
2019-10-18 19:56:49 -0300

    Makes topwords endpoint work

--- word-ranking/web/hello.py
+++ word-ranking/web/hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from bs4 import BeautifulSoup
+from collections import defaultdict
 import requests
 app = Flask(__name__)
 
@@ -19,5 +20,35 @@ def top_words():
 def get_words_from_url(url):
     response = requests.get(url)
     html = response.text
-    words = ['word1', 'word2']
-    return words
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.find_all(text=True)
+
+    words_dict = defaultdict(int)
+    blacklist = [
+        '[document]',
+        'noscript',
+        'header',
+        'html',
+        'meta',
+        'head',
+        'input',
+        'script',
+    ]
+
+    for t in text:
+        if t.parent.name not in blacklist:
+            words = t.split()
+            for word in words:
+                if word.isalnum():
+                    words_dict[word.lower()] += 1
+
+    sorted_words = sorted(words_dict.items(),
+                          key=lambda a:a[1],
+                          reverse=True
+                         )[:10]
+
+    sorted_words_dict = dict()
+    for word, count in sorted_words:
+        sorted_words_dict[word] = count
+
+    return sorted_words_dict
