698ac8b0d3dc055ff16a131bba4b8d46aeab0fdf
Tomas Sironi <sironitomas@gmail.com>
2019-10-17 23:00:27 -0300

    Updates hello.py

--- word-ranking/web/hello.py
+++ word-ranking/web/hello.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+from bs4 import BeautifulSoup
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api/topwords', methods = ['GET'])
+def top_words():
+    # to do: add error checking
+    url = request.args['url']
+    words = get_words_from_url(url)
+    return jsonify(words)
+
+def get_words_from_url(url):
+    response = requests.get(url)
+    html = response.text
+    words = ['word1', 'word2']
+    return words
